7c6b6329a367d8d8fdec82625c7859c9bd0f5b8d
PamekasanCode <69365920+pamekasancode@users.noreply.github.com>
2020-08-18 05:57:29 +0000

    Problemset #4A
    Catagories: Easy

---
+++ Python/Watermelon.py
@@ -0,0 +1,13 @@
+def solve():
+    w = int(input())
+
+    if w == 2:
+        print("NO")
+    else: 
+        if w % 2 == 0:
+            print("YES")
+        else:
+            print("NO")
+
+
+solve()
